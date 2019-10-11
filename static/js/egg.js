050f152ab39114b328f5dcd0f2dde705c573ff12
AccelianLLC <AccelianLLC@users.noreply.github.com>
2019-10-11 16:33:55 -0400

    Add files via upload

---
+++ static/js/egg.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#easter-bunny').on('click', function(){
+		$('.navbar-default').css('background-color', 'yellowgreen');
+		$('.small-block').css('background-color', 'yellowgreen');
+		$('.carousel-content').css('background-color', 'yellowgreen');
+		$('.content-header').css('background-color', 'yellowgreen');
+		$('body').css('background-color', 'forestgreen');
+		$('h3').css('background-color', 'yellowgreen');
+	});
+});
